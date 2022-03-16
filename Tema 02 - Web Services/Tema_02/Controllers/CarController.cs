95aa06fa644e18b22c61fae7fda0116f67215820
Iulian Ivanov <61800829+IulianIvanov@users.noreply.github.com>
2022-03-16 04:18:53 +0200

    Add files via upload

---
+++ Tema 02 - Web Services/Tema_02/Controllers/CarController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tema_02.IService;
+using Tema_02.Model;
+
+namespace Tema_02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : GenericController<Car>
+    {
+        public CarController(IGenericService<Car> genericService) : base(genericService)
+        {
+
+        }
+    }
+}
---
+++ Tema 02 - Web Services/Tema_02/Model/Costumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Tema_02.Model
+{
+    public class Costumer
+    {
+        public int CostumerId { get; set; } = 1;
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Car> Cars { get; set; }
+    }
+}
