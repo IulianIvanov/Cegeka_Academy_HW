623a296f4377295e21a666905c867ded937c1fbd
Iulian Ivanov <61800829+IulianIvanov@users.noreply.github.com>
2022-03-16 04:30:38 +0200

    Add files via upload

---
+++ Tema 02 - Web Services/Controllers/CostumerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tema_02.IService;
+using Tema_02.Model;
+
+namespace Tema_02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CostumerController : GenericController<Costumer>
+    {
+        public CostumerController(IGenericService<Costumer> genericService) : base(genericService)
+        {
+
+        }
+    }
+}
---
+++ Tema 02 - Web Services/Service/CostumerService.cs
@@ -0,0 +1,41 @@
+﻿using Tema_02.IService;
+using Tema_02.Model;
+
+namespace Tema_02.Service
+{
+    public class CostumerService : IGenericService<Costumer>
+    {
+        List<Costumer> _costumer = new List<Costumer>();
+        public CostumerService()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _costumer.Add(new Costumer()
+                {
+                    CostumerId = i,
+                    Name = "Name" + i,
+                });
+            }
+        }
+        public List<Costumer> Delete(int id)
+        {
+            _costumer.RemoveAll(x => x.CostumerId == id);
+            return _costumer;
+        }
+
+        public List<Costumer> GetAll()
+        {
+            return _costumer;
+        }
+        public Costumer GetById(int id)
+        {
+            return _costumer.Where(x => x.CostumerId == id).SingleOrDefault();
+        }
+
+        public List<Costumer> Insert(Costumer item)
+        {
+            _costumer.Add(item);
+            return _costumer;
+        }
+    }
+}
